7ae787efc97db9c13271b45cb9c269c71faba326
Felicia Dahl <feliciadahl01@gmail.com>
2025-11-04 15:49:30 +0100

    Add _CallbackForm with styling

--- OnatrixUmbraco/ViewModels/CallbackFormViewModel.cs
+++ OnatrixUmbraco/ViewModels/CallbackFormViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel.DataAnnotations;
 
 namespace OnatrixUmbraco.ViewModels;
 
@@ -18,5 +19,8 @@ public class CallbackFormViewModel
     public string Phone { get; set; } = null!;
     [Required(ErrorMessage = "Please select an option")]
     public string SelectedOption { get; set; } = null!;
+
+    [BindNever]
+    public IEnumerable<string> Options { get; set; } = [];
 
 }
