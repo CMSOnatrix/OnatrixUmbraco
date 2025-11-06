807c339990db478339e2c74252bebfb6a98e7b8b
Felicia Dahl <feliciadahl01@gmail.com>
2025-11-06 10:43:38 +0100

    Add FormSubmissionsService

--- OnatrixUmbraco/Controllers/FormController.cs
+++ OnatrixUmbraco/Controllers/FormController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnatrixUmbraco.Services;
 using OnatrixUmbraco.ViewModels;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Logging;
@@ -10,21 +11,26 @@ using Umbraco.Cms.Web.Website.Controllers;
 
 namespace OnatrixUmbraco.Controllers;
 
-public class FormController : SurfaceController
+public class FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider, FormSubmissionsService formSubmissionsService) : SurfaceController(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
 {
-    public FormController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
-    {
-    }
+    private readonly FormSubmissionsService _formSubmissionsService = formSubmissionsService;
+
 
     public IActionResult HandleCallBackForm(CallbackFormViewModel model)
     {
        if(!ModelState.IsValid)
         {
-            // If the model is not valid, return the current page with validation errors
+          
             return CurrentUmbracoPage();
         }
-        // Process the form data (e.g., save to database, send email, etc.)
-        // Redirect to a thank you page or display a success message
+    
+       var result = _formSubmissionsService.SaveCallbackRequest(model);
+        if(!result)
+        {
+
+            return RedirectToCurrentUmbracoPage();
+        }
+
         return RedirectToCurrentUmbracoPage();
     }
 }
