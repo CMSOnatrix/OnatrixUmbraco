8ffb1ec6a5bf177763a7db016432cd70f19cc00b
Felicia Dahl <feliciadahl01@gmail.com>
2025-11-10 16:22:15 +0100

    Add _Header.css with dropdown

---
+++ OnatrixUmbraco/wwwroot/js/nav.js
@@ -0,0 +1,10 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.querySelector(".nav-toggle");
+    const navList = document.querySelector(".nav-list");
+
+    if (toggle && navList) {
+        toggle.addEventListener("click", () => {
+            navList.classList.toggle("open");
+        });
+    }
+});
